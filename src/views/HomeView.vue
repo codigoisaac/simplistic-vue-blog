7b9a50448e21fbc4323a766c3528d017784d7f29
Isaac Muniz <isaacmuniz@protonmail.com>
2022-04-20 00:48:25 -0300

    Working with refs and binding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="home">
     <p ref="p">
-      We did it, Vue. My name is {{ name }} and my light is {{ light }}.
+      We did it, Vue. My name is {{ name }} and my light is {{ light }}+.
     </p>
 
     <button @click="handleClick">Ye</button>
+    <button @click="light++">+</button>
+
+    <input type="text" v-model="name" />
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
+
 export default {
   name: 'HomeView',
 
   setup() {
-    const name = 'Isaac'
-    const light = '99+'
+    const name = ref('Isaac')
+    const light = ref(99)
 
     const p = ref(null)
 
     const handleClick = () => {
       p.value.textContent = 'Ye'
+      name.value = name.value + ':)'
     }
 
     return { name, light, handleClick, p }
